19ed33074a83d42a31d42609e53ce51ace37dbe1
Mohannad Hisham <Mohannadize15@gmail.com>
2021-12-25 01:15:42 +0200

    Fix 4 gh-pages

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://raw.githubusercontent.com/mohannadize/3elm_bgd/master/docs/' : '',
+  assetPrefix: isProd ? './' : '',
 }
