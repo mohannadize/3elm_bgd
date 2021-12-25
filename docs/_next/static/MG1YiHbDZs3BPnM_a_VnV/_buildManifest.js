ba02378826d49cf338b61e83b58dbee90dfe89bf
Mohannad Hisham <Mohannadize15@gmail.com>
2021-12-25 18:34:42 +0200

    updated gh-pages

---
+++ docs/_next/static/MG1YiHbDZs3BPnM_a_VnV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/187-c956788ed197f7f2.js","static/chunks/pages/index-967cae9a30a2f801.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
